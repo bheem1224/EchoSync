6ba09cd2fcb9e8deda0b658fd7be96577c432f67
Abhay Doss <bheam12@gmail.com>
2026-06-28 09:55:38 -0400

    forgot to generate the working.db alembic migration

--- database/migrations/working/env.py
+++ database/migrations/working/env.py
@@ -21,6 +21,13 @@ if config.config_file_name is not None and config.attributes.get('configure_logg
 # add your model's MetaData object here
 # for 'autogenerate' support
 target_metadata = WorkingBase.metadata
+target_metadata.naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
 def run_migrations_offline() -> None:
     db = get_working_database()
